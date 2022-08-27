dfeed74b10a54da36807d9c2d6196f7c3a43d03f
Poxie <poxen12@hotmail.com>
2022-08-27 23:50:36 +0200

    Added login button in header

--- components/navbar/NavbarRight.tsx
+++ components/navbar/NavbarRight.tsx
@@ -1,10 +1,14 @@
 import styles from '../../styles/Navbar.module.scss';
+import Button from '../button';
 import { NavbarTabs } from './NavbarTabs';
 
 export const NavbarRight = () => {
     return(
         <div className={styles.right}>
             <NavbarTabs />
+            <Button>
+                Login with Spotify
+            </Button>
         </div>
     )
 }

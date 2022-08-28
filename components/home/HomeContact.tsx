c940ae670ebcb0de28962d2d074277520592257a
Poxie <poxen12@hotmail.com>
2022-08-28 03:39:19 +0200

    Added basic contact section

---
+++ components/home/HomeContact.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import Image from "next/image"
+import styles from '../../styles/Home.module.scss';
+import Button from "../button";
+import { Input } from "../input";
+
+export const HomeContact = () => {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [statusMessage, setStatusMessage] = useState('');
+    const [statusCode, setStatusCode] = useState(200);
+
+    const submit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Making sure inputs arent empty
+        if(!message || !email) return;
+
+        // Resetting mail inputs
+        setMessage('');
+        setEmail('');
+
+        // Sending mail 
+        fetch(`/api/message`, {
+            method: 'POST',
+            body: JSON.stringify({
+                message,
+                email
+            })
+        })
+        .then(res => res.json())
+        .then(({ message, status }) => {
+            // Updating mail status
+            setStatusMessage(message);
+            setStatusCode(status);
+        })
+    }
+
+    const statusClassName = [
+        styles['contact-status'],
+        statusCode === 200 ? styles['success'] : styles['error']
+    ].join(' ');
+    return(
+        <section className={styles.contact}>
+            <div 
+                style={{ bottom: 'unset', top: -50 }}
+                className={styles.separator} 
+                aria-hidden="true"
+            >
+                <Image 
+                    src={'/icons/separator.svg'}
+                    layout={'fill'}
+                    objectFit={'cover'}
+                />
+            </div>
+
+            <div className={styles['contact-content']}>
+                <h2>
+                    Feel like something's missing?
+                </h2>
+                <form onSubmit={submit}>
+                    <Input 
+                        label={'Email'}
+                        type={'email'}
+                        onChange={setEmail}
+                        value={email}
+                    />
+                    <Input 
+                        label={'Message'}
+                        textArea={true}
+                        onChange={setMessage}
+                        value={message}
+                    />
+                    <Button 
+                        type={'secondary'}
+                        className={styles['contact-button']}
+                    >
+                        Send Message
+                    </Button>
+                </form>
+                {statusMessage && (
+                    <span className={statusClassName}>
+                        {statusMessage}
+                    </span>
+                )}
+            </div>
+        </section>
+    )
+}
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from '../../styles/Home.module.scss';
+import { HomeContact } from './HomeContact';
 import { HomeHeader } from './HomeHeader';
 import { HomeTiles } from './HomeTiles';
 
@@ -8,6 +9,7 @@ export const Home = () => {
         <main>
             <HomeHeader />
             <HomeTiles />
+            <HomeContact />
         </main>
     )
 }

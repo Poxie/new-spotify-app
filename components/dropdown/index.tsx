c29f02ca26e2a816f5d7bd1e4ba33e5aca7d76c4
Poxie <poxen12@hotmail.com>
2022-08-28 23:00:37 +0200

    Created dropdown component

---
+++ components/dropdown/index.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
+import { Input } from '../input';
+import styles from './Dropdown.module.scss';
+
+export const Dropdown: React.FC<{
+    items: string[];
+    onChange: (item: string) => void;
+    defaultSelected?: string;
+    allowSearch?: boolean;
+}> = ({ items, onChange, defaultSelected, allowSearch }) => {
+    const [selected, setSelected] = useState(defaultSelected || items[0]);
+    const [search, setSearch] = useState('');
+    const [open, setOpen] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+
+    // Selecting new item
+    const selectItem = (item: string) => {
+        setSelected(item);
+        onChange(item);
+        setOpen(false);
+        setSearch('');
+    }
+
+    // Closing on click outside component
+    const checkForClickOutside = (e: MouseEvent) => {
+        // @ts-ignore: this works
+        if(ref.current && !ref.current.contains(e.target)) {
+            setOpen(false);
+        }
+    }
+    useEffect(() => {
+        document.addEventListener('mousedown', checkForClickOutside);
+        return () => document.removeEventListener('mousedown', checkForClickOutside);
+    }, []);
+
+    const selectableItems = !search ? items : items.filter(item => item.toLowerCase().includes(search.toLowerCase()))
+    return(
+        <div className={styles['container']} ref={ref}>
+            <button 
+                className={styles['selected']}
+                onClick={() => setOpen(!open)}
+            >
+                {selected}
+            </button>
+
+            {open && (
+                <ul>
+                    <Input 
+                        label={'Search'}
+                        name="search"
+                        containerClassName={styles['search']}
+                        onChange={setSearch}
+                    />
+                    {selectableItems.map(item => (
+                        <li 
+                            onClick={() => selectItem(item)}
+                            key={item}
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}

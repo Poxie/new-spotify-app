33d06685d5c7cab2cad6e67df62cfffc5c6dfba7
Poxie <poxen12@hotmail.com>
2022-08-30 01:50:04 +0200

    Changing navbar color on different paths

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,10 +1,18 @@
+import { useRouter } from 'next/router'
 import styles from '../../styles/Navbar.module.scss'
 import { NavbarHeader } from './NavbarHeader'
 import { NavbarRight } from './NavbarRight'
 
+const LIGHT_PATHS = ['/'];
 export const Navbar = () => {
+    const path = useRouter().asPath;
+
+    const className = [
+        styles['container'],
+        LIGHT_PATHS.includes(path) ? styles['light'] : ''
+    ].join(' ');
     return(
-        <header className={styles.container}>
+        <header className={className}>
             <nav className={styles.content}>
                 <NavbarHeader />
                 <NavbarRight />

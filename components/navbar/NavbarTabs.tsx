269b5c0275cd0ff9673e008442b41e8bf3e904c1
Poxie <poxen12@hotmail.com>
2022-08-31 00:35:54 +0200

    Added active state to navbar tabs

--- components/navbar/NavbarTabs.tsx
+++ components/navbar/NavbarTabs.tsx
@@ -1,17 +1,32 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../../styles/Navbar.module.scss';
 
 const tabs = ['Top Lists', 'Explore', 'Profile'];
 export const NavbarTabs = () => {
+    const asPath = useRouter().asPath;
+
     return(
         <ul className={styles.tabs}>
-            {tabs.map(tab => (
-                <li className={styles.tab} key={tab}>
-                    <Link href={tab.toLowerCase().replaceAll(' ', '-')}>
-                        {tab}
-                    </Link>
-                </li>
-            ))}
+            {tabs.map(tab => {
+                const path = `/${tab.toLowerCase().replaceAll(' ', '-')}`;
+                const active = path === asPath;
+
+                const className = [
+                    styles['tab'],
+                    active ? styles['active'] : ''
+                ].join(' ');
+                return(
+                    <li 
+                        className={className} 
+                        key={tab}
+                    >
+                        <Link href={path}>
+                            {tab}
+                        </Link>
+                    </li>
+                )
+            })}
         </ul>
     )
 }

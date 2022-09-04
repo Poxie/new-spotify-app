3174f257a24c37ae54360d24d329c13ef2d9b8ee
Poxie <poxen12@hotmail.com>
2022-09-04 19:22:42 +0200

    Making sure multiple requests arent being made

--- components/profile/ProfileTops.tsx
+++ components/profile/ProfileTops.tsx
@@ -1,21 +1,25 @@
 import styles from '../../styles/Profile.module.scss';
-import { selectProfileActiveArtistTerm, selectProfileActiveTracksTerm, selectProfileArtists, selectProfileTracks } from "../../redux/profile/hooks"
+import { selectProfileActiveArtistTerm, selectProfileActiveTracksTerm, selectProfileArtists, selectProfileToken, selectProfileTracks } from "../../redux/profile/hooks"
 import { useAppSelector } from "../../redux/store"
 import { ProfileTop } from "./ProfileTop";
 import { useAuth } from '../../contexts/auth/AuthProvider';
 import { useDispatch } from 'react-redux';
 import { setProfileArtists, setProfileTracks } from '../../redux/profile/action';
+import { useEffect } from 'react';
 
 export const ProfileTops = () => {
     const { get } = useAuth();
     const dispatch = useDispatch();
+    const token = useAppSelector(state => selectProfileToken(state, 'access_token'));
     const tracks = useAppSelector(selectProfileTracks);
     const artists = useAppSelector(selectProfileArtists);
     const activeTrackTerm = useAppSelector(selectProfileActiveTracksTerm);
     const activeArtistTerm = useAppSelector(selectProfileActiveArtistTerm);
 
     // If tracks does not exist, fetch them
-    if(!tracks) {
+    useEffect(() => {
+        if(tracks || !token) return;
+
         get(`me/top/tracks?time_range=${activeTrackTerm}&limit=50`)
             .then(({ items }) => {
                 dispatch(setProfileTracks({
@@ -23,9 +27,12 @@ export const ProfileTops = () => {
                     items
                 }))
             }).catch(console.error)
-    }
+    }, [activeTrackTerm, token]);
+
     // If artists dont exist, fetch them
-    if(!artists) {
+    useEffect(() => {
+        if(artists || !token) return;
+
         get(`me/top/artists?time_range=${activeArtistTerm}&limit=50`)
             .then(({ items }) => {
                 dispatch(setProfileArtists({
@@ -33,7 +40,7 @@ export const ProfileTops = () => {
                     items
                 }))
             }).catch(console.error)
-    }
+    }, [activeArtistTerm, token]);
 
     return(
         <>
